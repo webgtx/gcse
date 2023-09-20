898afa5646f1fef204ac8c4e4688b17790b3a8f2
David Jhonson <alx.zolotarov@gmail.com>
2023-09-20 08:52:26 +0100

    Update: added new job into the script

--- main.sh
+++ main.sh
@@ -1,12 +1,22 @@
 #!/usr/env bash
 
-export BRANCH="master"
-export STORAGE_URL="https://raw.githubusercontent.com/webgtx/gcse/$branch/"
+BRANCH="master"
+STORAGE_URL="https://raw.githubusercontent.com/webgtx/gcse/$branch/"
+TS_ARCH="386"
+TS_RELEASE_URL="https://pkgs.tailscale.com/stable/tailscale_1.48.2_$TS_ARCH.tgz"
 
 if [[ $USER != "root" ]]; then
   echo "run script as root"
   exit 1
 fi
 
-curl $storage_url/configs/sshd_config -o /etc/ssh/sshd_config
+# Setup SSHD
+curl $STORAGE_URL/configs/sshd_config -o /etc/ssh/sshd_config
 /usr/sbin/sshd
+
+# Connect to tailscale network
+wget TS_RELEASE_URL
+tar xf tailscale*
+cd tailscale*
+nohup ./tailscaled > /dev/null > 2>&1
+
