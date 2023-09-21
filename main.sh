0ae65fbfb836c9c45e19c3be0a556771ab2ad618
David Jhonson <alx.zolotarov@gmail.com>
2023-09-21 03:05:41 +0100

    Fixes: variables access

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-BRANCH="master"
-STORAGE_URL="https://raw.githubusercontent.com/webgtx/gcse/$BRANCH"
-TS_ARCH="386"
-TS_RELEASE_URL="https://pkgs.tailscale.com/stable/tailscale_1.48.2_$TS_ARCH.tgz"
+export BRANCH="master"
+export STORAGE_URL="https://raw.githubusercontent.com/webgtx/gcse/$BRANCH"
+export TS_ARCH="386"
+export TS_RELEASE_URL="https://pkgs.tailscale.com/stable/tailscale_1.48.2_$TS_ARCH.tg"
 
 # Setup SSHD
 sudo curl "$STORAGE_URL/sshd.sh" | sudo bash
